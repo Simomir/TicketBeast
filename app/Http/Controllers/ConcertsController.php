55c52da85cc6570589d8bab3fa086be81a8da3f2
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-04 17:50:33 +0300

    scafolding view

--- app/Http/Controllers/ConcertsController.php
+++ app/Http/Controllers/ConcertsController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Concert;
 use Illuminate\Http\Request;
 
 class ConcertsController extends Controller
 {
-    //
+    public function show(Concert $concert) {
+        return view('concerts.show')->with('concert', $concert);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConcertsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/concerts/{id}', [ConcertsContoller::class, 'show']);
+Route::get('/concerts/{concert}', [ConcertsController::class, 'show']);
 
