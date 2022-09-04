a142401c45938dcc79e9f3ff70262a234863ba7d
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-04 16:38:43 +0300

    Initial

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewConcertListingTest extends TestCase
+{
+    /** @test */
+    function user_can_view_a_concert_listing() {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+    }
+}
