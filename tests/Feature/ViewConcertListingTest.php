b47eebac075a9e8b270d22bc2c2baf30af0d104a
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-04 16:47:55 +0300

    create concert instanse in test

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -10,6 +11,18 @@ class ViewConcertListingTest extends TestCase
     /** @test */
     function user_can_view_a_concert_listing() {
         //Arrange
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-555.'
+        ]);
 
         //Act
 
