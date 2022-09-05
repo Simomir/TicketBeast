43f592a2d7e993a18da808a7d5ed761da6df55db
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-05 16:23:16 +0300

    optimized tests speed

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -11,11 +11,10 @@ use Tests\TestCase;
 
 class ConcertTest extends TestCase
 {
-    use RefreshDatabase;
 
     /** @test */
     function can_get_formatted_date() {
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
 
@@ -24,7 +23,7 @@ class ConcertTest extends TestCase
 
     /** @test */
     function can_get_formatted_start_time() {
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->make([
             'date' => Carbon::parse('2016-12-01 17:00:00')
         ]);
 
@@ -33,7 +32,7 @@ class ConcertTest extends TestCase
 
     /** @test */
     function can_get_ticket_price_in_dollars() {
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->make([
             'ticket_price' => 6750
         ]);
 
