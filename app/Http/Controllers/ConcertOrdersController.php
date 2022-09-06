4fa194da66ce0c63f21eff7f6382bfb1c7b21dd0
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-06 11:48:32 +0300

    dummy logic in new controller

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class ConcertOrdersController extends Controller
 {
-    //
+    public function store() {
+        return response()->json([], 201);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ConcertOrdersController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConcertsController;
 
