181cdaade59a8061383d7017fe3ea44c118c6a20
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-06 10:51:36 +0300

    scafolded the new test

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Concert;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,6 +11,18 @@ class PurchaseTicketsTest extends TestCase
 {
     /** @test */
     function customer_can_purchase_concert_tickets() {
+        $concert = Concert::factory()->create(['ticket_price' => 3250]);
 
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => $paymentGateway->getValidTestToken(),
+        ]);
+
+        $this->assertEquals(9750, $paymentGateway->totalCharges());
+
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNotNull($order);
+        $this->assertEquals(3, $order->tickets->count());
     }
 }
