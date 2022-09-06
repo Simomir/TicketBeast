33228cd0f8a6329ecfc36b06c538f8f91e124374
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-06 11:44:51 +0300

    new route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use App\Http\Controllers\ConcertsController;
 
 Route::get('/concerts/{id}', [ConcertsController::class, 'show']);
 
+Route::post('/concerts/{id}/orders', [ConcertOrdersController::class, 'store']);
