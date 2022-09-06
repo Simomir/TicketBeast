b52efeffb851b62de5fc4d36a9a808726d8dcfbf
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-06 11:22:32 +0300

    scafolding test

--- tests/Feature/FakPaymentGatewayTest.php
+++ tests/Feature/FakPaymentGatewayTest.php
@@ -2,21 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Billing\FakePaymentgateway;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class FakPaymentGatewayTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    /** @test */
+    function charges_with_a_valid_payment_token_are_successful() {
+        $paymentGateway = new FakePaymentgateway;
 
-        $response->assertStatus(200);
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+
+        $this->assertEquals(2500, $paymentGateway->totalCharges());
     }
 }
