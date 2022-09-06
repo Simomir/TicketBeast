a24126f33c03f455baf8d1391b995914e264c3a4
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-06 10:35:44 +0300

    create new test

---
+++ tests/Feature/PurchaseTicketsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PurchaseTicketsTest extends TestCase
+{
+    /** @test */
+    function customer_can_purchase_concert_tickets() {
+
+    }
+}
