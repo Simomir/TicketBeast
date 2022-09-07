8635b026bd8c34b4a9502c9f13ab7e6441bf372f
Simeon Shopov <simmeon@slavovstudio.bg>
2022-09-07 15:00:29 +0300

    fixed not working test

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -25,11 +25,7 @@ class ConcertOrdersController extends Controller
         ]);
 
         $concert = Concert::find($id);
-        $order = $concert->orders()->create(['email' => $request->input('email')]);
-
-        foreach (range(1, $request->input('ticket_quantity')) as $i) {
-            $order->tickets()->create([]);
-        }
+        $order = $concert->orderTickets($request->input('email'), $request->input('ticket_quantity'));
 
         $this->paymentGateway->charge($request->input('ticket_quantity') * $concert->ticket_price, $request->input('payment_token'));
 
