0387cf349f2d3fb60df9c2afa5e0a7d012e418fe
Simeon Shopov <simmeon@slavovstudio.bg>
2022-09-07 17:39:36 +0300

    if check for valid token

--- app/Billing/FakePaymentgateway.php
+++ app/Billing/FakePaymentgateway.php
@@ -20,6 +20,10 @@ class FakePaymentgateway implements PaymentGateway {
     }
 
     public function charge($amount, $token) {
+        if ($token !== $this->getValidTestToken()) {
+            throw new PaymentFailedException;
+        }
+
         $this->charges[] = $amount;
     }
 
