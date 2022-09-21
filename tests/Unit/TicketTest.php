c335d4c6bde6569f5f775ae5edd967ffaf1508e5
Simeon Shopov <simmeon@slavovstudio.bg>
2022-09-21 15:08:48 +0300

    ticket test logic

--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -10,9 +10,20 @@ use Tests\TestCase;
 
 class TicketTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function a_ticket_can_be_released()
     {
+        $concert = Concert::factory()->create();
+        $concert->addTickets(1);
 
+        $order = $concert->orderTickets('jane@example.com', 1);
+        $ticket = $order->tickets()->first();
+
+        $this->assertEquals($order->id, $ticket->order_id);
+
+        $ticket->release();
+        $this->assertNull($ticket->fresh()->order_id);
     }
 }
