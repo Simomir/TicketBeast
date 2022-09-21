e5be171bec907602ceeb5c038ba99f9bc0e47763
Simeon Shopov <simmeon@slavovstudio.bg>
2022-09-21 14:14:18 +0300

    order test logic

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace Tests\Unit;
+
+use App\Models\Concert;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class OrderTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function tickets_are_released_when_order_is_cancelled()
+    {
+        $concert = Concert::factory()->create();
+        $concert->addTickets(10);
+        $order = $concert->orderTickets('jane@example.com', 5);
+
+        // pre-condition
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+
+
+    }
+}
