83c55a03e84aa4c99024f0d9cf1c26316966913a
Simeon Shopov <simmeon@slavovstudio.bg>
2022-09-21 15:00:47 +0300

    new unit test file

---
+++ tests/Unit/TicketTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Concert;
+use App\Models\Order;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class TicketTest extends TestCase
+{
+    /** @test */
+    function a_ticket_can_be_released()
+    {
+
+    }
+}
